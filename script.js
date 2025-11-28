644442f7dc17f6dc332228bab90530d29607b553
Leontiadis Konstantinos <113901432+LeonKonst@users.noreply.github.com>
2025-11-28 13:26:02 +0100

    Fix bug of lowercase first later on book genre and include more functionality in the newBook class

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const bookcardContainer = document.querySelector(".bookcard-container");
-const addBookBtn = document.querySelector(".add-book-btn");
-
 class Library {
 
     constructor(){
@@ -20,9 +17,6 @@ class Library {
         UIhandler.refreshBookshelf ()
     }
 }
-
-const library = new Library();
-
 
 class Book {
 
@@ -79,64 +73,72 @@ class UIhandler {
 
 class UIBookCards{
         static addNewBookcard(){
-        bookcardContainer.innerHTML += `
-            <div class="new-bookcard">
-                <button class="delete-book-btn">x</button>
-                <button class="accept-new-book-btn">✓</button>
-                <div>
-                    <label for="new-book-title">Book Title</label>
-                    <input class="new-book-title" type="text" id="new-book-title">
-                </div>
 
-                    <div>
-                    <label for="new-book-author">Author</label>
-                    <input class="new-book-author" type="text" id="new-book-author">
-                </div>
+        const cardContainer = document.createElement("div");
+        cardContainer.classList = "new-bookcard";
+        bookcardContainer.appendChild(cardContainer);
+        cardContainer.innerHTML += `
+            <button class="delete-book-btn">x</button>
+            <button class="accept-new-book-btn">✓</button>
+            <div>
+                <label for="new-book-title">Book Title</label>
+                <input class="new-book-title" type="text" id="new-book-title">
+            </div>
 
                 <div>
-                    <label for="new-book-year">Release Year</label>
-                    <input class="new-book-year" type="number" id="new-book-year">
-                </div>
+                <label for="new-book-author">Author</label>
+                <input class="new-book-author" type="text" id="new-book-author">
+            </div>
 
-                <div>
-                    <label for="new-book-pages">Pages</label>
-                    <input class="new-book-pages" type="number" id="new-book-pages">
-                </div>
+            <div>
+                <label for="new-book-year">Release Year</label>
+                <input class="new-book-year" type="number" id="new-book-year">
+            </div>
 
-                <div>
-                    <label for="genre">Genre</label>
-                    <select class="new-book-genre" name="genre" id="genre">
-                        <option value="fiction">Fiction</option>
-                        <option value="nonfiction">Nonfiction</option>
-                        <option value="poetry">Poetry</option>
-                        <option value="drama">Drama</option>
-                    </select>
-                </div>
-                <div>
-                    <label for="new-has-been-read">Have you read it?</label>
-                    <select class="new-has-been-read" name="new-has-been-read" id="new-has-been-read">
-                        <option value="true">Yes</option>
-                        <option value="false">No</option>
-                    </select>
-                </div>
-                
+            <div>
+                <label for="new-book-pages">Pages</label>
+                <input class="new-book-pages" type="number" id="new-book-pages">
+            </div>
+
+            <div>
+                <label for="genre">Genre</label>
+                <select class="new-book-genre" name="genre" id="genre">
+                    <option value="fiction">Fiction</option>
+                    <option value="nonfiction">Nonfiction</option>
+                    <option value="poetry">Poetry</option>
+                    <option value="drama">Drama</option>
+                </select>
+            </div>
+            <div>
+                <label for="new-has-been-read">Have you read it?</label>
+                <select class="new-has-been-read" name="new-has-been-read" id="new-has-been-read">
+                    <option value="true">Yes</option>
+                    <option value="false">No</option>
+                </select>
             </div>
         `;
     }    
 
+    static capitalizeFirstLetter(val) {
+        return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+    }
 
     static acceptNewbookData(){
         const bookTitleInput = document.querySelector(".new-book-title").value;
         const authorNameInput = document.querySelector(".new-book-author").value;
         const releaseYearInput = document.querySelector(".new-book-year").value;
         const pagesInput = document.querySelector(".new-book-pages").value;
-        const genreInput = document.querySelector(".new-book-genre").value;
+        const genreInput = UIBookCards.capitalizeFirstLetter(document.querySelector(".new-book-genre").value);
         const hasBeenReadInput = document.querySelector(".new-has-been-read").value==="true";
         library.addBookToLibrary(bookTitleInput, authorNameInput, releaseYearInput, pagesInput, genreInput, hasBeenReadInput);
         addBookBtn.disabled = false;
     }
 }
 
+const bookcardContainer = document.querySelector(".bookcard-container");
+const addBookBtn = document.querySelector(".add-book-btn");
+
+const library = new Library();
 
 library.addBookToLibrary("The Great Gatsby","F. Scott Fitzgerald",1925,218,"Fiction", true);
 library.addBookToLibrary("1984","George Orwell",1949,328, "Fiction", false);
